9df288dc2c958b3264140ca58311013c89391e7e
Mustapha Mele mustapha <mustaphamelemustapha@Mustaphas-MacBook-Air.local>
2026-04-23 07:22:29 +0100

    fix: make alembic import backend package

--- alembic/env.py
+++ alembic/env.py
@@ -1,3 +1,10 @@
+import sys
+from pathlib import Path
+
+ROOT = Path(__file__).resolve().parents[1]
+if str(ROOT) not in sys.path:
+    sys.path.insert(0, str(ROOT))
+
 from logging.config import fileConfig
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
