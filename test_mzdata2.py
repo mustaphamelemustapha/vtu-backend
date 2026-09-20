7a2143450d8fd58443756760f79b38858976720e
Mustapha Mele Mustapha <mustaphamelemustapha@gmail.com>
2026-09-20 12:46:30 +0100

    test: add script to test mzdata purchase API endpoint

---
+++ test_mzdata2.py
@@ -0,0 +1,13 @@
+import httpx
+
+url = "https://mzdata-1.onrender.com/api/v1/developer/data/purchase"
+payload = {
+    "network": "mtn",
+    "phone_number": "09095263835",
+    "plan_id": "423",
+    "reference": "TEST_1234567"
+}
+
+resp = httpx.post(url, json=payload, headers={"Authorization": "Bearer fake"})
+print(resp.status_code)
+print(resp.text)
